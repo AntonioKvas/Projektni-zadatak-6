30529ff19be60dc179b8b178775266e015c1b6e5
[Antonio <[akvas@tvz.hr]>
2023-05-23 18:14:20 +0200

    origin

---
+++ projektni zadatak 6/korisnik/ispis_korisnika.py
@@ -0,0 +1,6 @@
+def get_korisnik(redni_broj, korisnik):
+    return f"{redni_broj}.  {korisnik.email} {korisnik.telefon}"
+
+def ispis_svih_korisnika(korisnici):
+    for korisnik in korisnici:
+        korisnik.ispis()
---
+++ projektni zadatak 6/korisnik/unos_korisnika.py
@@ -0,0 +1,25 @@
+from utilities import unos_telefona, unos_intervala
+from .poslovni_korisnik import PoslovniKorisnik
+from .privatni_korisnik import PrivatniKorisnik
+
+def unos_korisnika(redni_broj):
+
+    email = input(f'Unesite email {redni_broj}. korisnika: ').strip()
+    telefon = unos_telefona(f'Unesite telefon {redni_broj}. korisnika: ')
+
+    print('Tipovi korisnika:')
+    print('\t 1. Poslovni korisnik')
+    print('\t 2. Privatni korisnik')
+    tip = unos_intervala(1, 2)
+
+    if tip == 1:
+        naziv = input(f'Unesite naziv {redni_broj}. korisnika: ')
+        web = input(f'Unesite web stranicu {redni_broj}. korisnika: ')
+
+        return PoslovniKorisnik(naziv, web, telefon, email)
+
+    elif tip == 2:
+        ime = input(f'Unesite ime {redni_broj}. korisnika: ').capitalize()
+        prezime = input(f'Unesite prezime {redni_broj}. korisnika: ').capitalize()
+
+        return PrivatniKorisnik(ime, prezime, telefon, email)
